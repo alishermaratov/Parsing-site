a60b9004ba3d7e6f422817c5524291a325380c30
Alisher <alishalish-777@mail.ru>
2020-12-12 21:24:28 +0600

    [ADD] парсинг авито вместе с старницами пагинации

--- avito.py
+++ avito.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from lxml import *
+import csv
 
 # план
 # 1 количество страниц
@@ -21,6 +22,58 @@ def get_total_pages(html):
 
     return int(total_pages)#функция возвращает количество страницы
 
+def write_csv(data):
+    with open('avito.csv', 'a',) as f:
+        writer = csv.writer(f, delimiter=';', lineterminator='\n')
+
+        writer.writerow((data['title'],
+                          data['price'],
+                          data['metro'],
+                          data['url']))
+
+
+
+def get_page_data(html):
+    soup = BeautifulSoup(html, 'lxml')
+
+    ads = soup.find('div',class_="items-items-38oUm").find_all('div', class_="iva-item-root-G3n7v photo-slider-slider-15LoY iva-item-list-2_PpT items-item-1Hoqq items-listItem-11orH js-catalog-item-enum")
+
+    for ad in ads:
+        # title, price, metro, url
+        name = ad.find('div', class_='iva-item-body-NPl6W').find('div', class_='iva-item-titleStep-2bjuh').text.strip().lower()
+
+
+        if 'samsung' in name:
+
+            try:
+                title = ad.find('div', class_='iva-item-body-NPl6W').find('div', class_='iva-item-titleStep-2bjuh').text.strip()
+            except:
+                title = ''
+
+            try:
+                url = 'https://www.avito.ru' + ad.find('div', class_='iva-item-body-NPl6W').find('div', class_='iva-item-titleStep-2bjuh').find('a').get('href')
+            except:
+                url = ''
+
+            try:
+                price = ad.find('div', class_='iva-item-priceStep-2qRpg').find('meta', itemprop ="price").get('content').text.strip()
+            except:
+                price = ''
+
+            try:
+                metro = ad.find('div', class_='geo-root-1pUZ8 iva-item-geo-1Ocpg').find_all('span')[1].text.strip()
+            except:
+                metro = ''
+
+            data = {'title': title,
+                    'price': price,
+                    'metro': metro,
+                    'url': url}
+
+            write_csv(data)
+        else:
+            continue
+
 def main():
     url = 'https://www.avito.ru/moskva/telefony/samsung-ASgBAgICAUSeAugJ?p=1&q=samsung'
     base_url = 'https://www.avito.ru/moskva/telefony/samsung-ASgBAgICAUSeAugJ?'
@@ -29,9 +82,11 @@ def main():
 
     total_pages = get_total_pages(get_html(url))
 
-    for i in range(1, total_pages ):
+    for i in range(1, 5):
         url_gen = base_url + page_part + str(i) + query_part
-        print(url_gen)
+        # print(url_gen)
+        html = get_html(url_gen)
+        get_page_data(html)
 
 
 
