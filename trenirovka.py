da26ee448c208fd77d4353cd9d3e321ed77564d5
Alisher <alishalish-777@mail.ru>
2021-01-07 15:21:27 +0600

    [ADD] сортировки

--- trenirovka.py
+++ trenirovka.py
@@ -345,7 +345,775 @@ def buble_sort(A):
 
 
 
+def choise_sort(A):
+    N=len(A)
+    for pos in range(0, N-1):
+        for k in range(pos+1, N):
+            if A[k]<A[pos]:
+                A[k], A[pos]=A[pos], A[k]
+    return A
 
+
+
+def insert_sort(A):
+    N=len(A)
+    for top in range(1, N):
+        k=top
+        while k>0 and A[k-1]>A[k]:
+            A[k], A[k-1] = A[k-1], A[k]
+            k-=1
+    return A
+
+def insert_sort(A):
+    N=len(A)
+    for top in range(1, N):
+        k=top
+        while k > 0 and A[k-1] > A[k]:
+            A[k], A[k-1] = A[k-1], A[k]
+            k-=1
+    return A
+
+
+
+
+def buble_sort(A):
+    N=len(A)
+    for i in range(1, N):
+        for k in range(0, N+i):
+            if A[k]>A[k+1]:
+                A[k], A[k+1]=A[k+1], A[k]
+    return A
+
+        def buble_sort(A):
+            N=Len(A)
+            for i in range(1, N):
+                for k in range(0, N-i):
+                    if A[k]>A[k+1]:
+                        A[k], A[k+1]=A[k+1], A[k]
+            return A
+
+
+def buble_sort(A):
+    N=len(A)
+    for i in range(1, N):
+        for k in range(0, N-i):
+            if A[k]>A[k+1]:
+                A[k], A[k+1] = A[k+1], A[k]
+    return A
+
+
+
+    def insert_sort(A):
+        N=len(A)
+        for top in range(1, N):
+            k=top
+            while k>0 and A[k-1]>A[k]:
+                A[k], A[k-1]= A[k-1], A[k]
+                k-=1
+        return A
+
+
+def insert_sort(A):
+    N=len(A)
+    for top in range(1, N):
+        k=top
+        while k > 0 and A[k-1] > A[k]:
+            A[k], A[k-1] = A[k-1], A[k]
+            k-=1
+    return A
+
+
+
+def choise_sort(A):
+    N=len(A)
+    for pos in range(0, N-1):
+        for k in range(pos+1, N):
+            if A[k]<A[pos]:
+                A[k], A[pos] = A[pos], A[k]
+    return A
+
+
+
+
+def choise_sort(A):
+    N=len(A)
+    for pos in range(0, N-1):
+        for k in range(pos+1, N):
+            if A[k]<A[pos]:
+                A[k], A[pos] = A[pos], A[k]
+    return A
+
+
+
+
+
+def binary_serch(A:list, key:int, start:int, stop:int):
+    if start>stop:
+        return False
+    middle=(start+stop)//2
+    if key == A[middle]:
+        return middle
+    elif key<A[middle]:
+        return binary_serch(A, key, start, middle+1)
+    else:
+        return binary_serch(A, key, middle+1, stop)
+
+
+
+
+
+def binary_search(A:list, key:int, start:int, stop:int):
+    if start > stop:
+        return False
+    middle = (start+stop)//2
+    if key == A[middle]:
+        return middle
+    elif key < A[middle]:
+         return binary_search(A, key, start, middle-1)
+    else:
+        return binary_search(A, key, middle+1, stop)
+
+
+
+def binary_search(A:list, key:int, start:int, stop:int):
+    if start>stop:
+        return False
+    middle= (start+stop)//2
+    if key==A[middle]:
+        return middle
+    elif key < A[middle]:
+        return binary_search(A, key, start, middle+1)
+    else:
+        return binary_search(A, key, middle+1, stop)
+
+
+
+
+def binary_search(A:list, key:int, start:int, stop:int):
+    if start > stop:
+        return False
+    middle = (start+stop)//2
+    if key == A[middle]:
+        return middle
+    elif key < A[middle]:
+         return binary_search(A, key, start, middle-1)
+    else:
+        return binary_search(A, key, middle+1, stop)
+
+
+
+
+def binary_search(A:list, key:int, start:int, stop:int):
+    if start>stop:
+        return False
+    middle=(start+stop)//2
+    if key==A[middle]:
+        return middle
+    elif key < A[middle]:
+        return binary_search(A, key, start, middle+1)
+    else:
+        return binary_search(A, key, middle+1, stop)
+
+
+
+def binary_search(A:list, key:int, start:int, stop:int):
+    if start > stop:
+        return False
+    middle = (start+stop)//2
+    if key == A[middle]:
+        return middle
+    elif key < A[middle]:
+        return binary_search(A, key, start, middle-1)
+    else:
+        return binary_search(A, key, middle+1, stop)
+
+
+
+def binary_search(A:list, key:int, start:int, stop:int):
+    if start>stop:
+        return False
+    middle=(start+stop)//2
+    if key==A[middle]:
+        return middle
+    elif key < A[middle]:
+        return binary_search(A, key, start, middle+1)
+    else:
+        return binary_search(A, key, middle+1, stop)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def binary_search(A:list, key:int, start:int, stop:int):
+    if start > stop:
+        return False
+    middle = (start+stop)//2
+    if key == A[middle]:
+        return middle
+    elif key < A[middle]:
+        return binary_search(A, key, start, middle-1)
+    else:
+        return binary_search(A, key, middle+1, stop)
+
+
+
+
+
+def binary_search(A:list, key:int, start:int, stop:int):
+    if start>stop:
+        return False
+    middle=(start+stop)//2
+    if key==A[middle]:
+        return middle
+    elif key<A[middle]:
+        return binary_search(A, key, start, middle-1)
+    else:
+        return binary_search(A, key, middle+1, stop)
+
+
+def insert_sort(A):
+    N=len(A)
+    for top in range(1, N):
+        k=top
+        while k>0 and A[k-1] > A[k]:
+            A[k], A[k-1]=A[k-1], A[k]
+            k-=1
+    return A
+
+def insert_sort(A):
+    N=len(A)
+    for top in range(1, N):
+        k=top
+        while k > 0 and A[k-1] > A[k]:
+            A[k], A[k-1] = A[k-1], A[k]
+            k-=1
+    return A
+
+
+def insert_sort(A):
+    N=len(A)
+    for top in range(1, N):
+        k=top
+        while k>0 and A[k-1]>A[k]:
+            A[k], A[k-1]=A[k-1], A[k]
+            k-=1
+    return A
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def choise_sort(A):
+    N=len(A)
+    for pos in range(0, N-1):
+        for k in range(pos+1, N):
+            if A[k]<A[pos]:
+                A[k], A[pos] = A[pos], A[k]
+    return A
+
+
+
+def choise_sort(A):
+    N=len(A)
+    for pos in range(0, N-1):
+        for k in range(pos+1, N):
+            if A[k]<A[pos]:
+                A[k], A[pos]=A[pos], A[k]
+    return A
+
+
+
+def choise_sort(A):
+    N=len(A)
+    for pos in range(0, N-1):
+        for k in range(pos+1, N):
+            if A[k]<A[pos]:
+                A[k], A[pos]=A[pos], A[k]
+    return A
+
+
+
+def buble_sort(A):
+    N=len(A):
+    for i in range(0, N):
+        for k in range(1, N+i):
+            if A[k]>A[k+1]:
+                A[k], A[k+1]=A[k+1], A[k]
+    return A
+
+    def buble_sort(A):
+        N=len(A)
+        for i in range(1, N):
+            for k in range(0, N-i):
+                if A[k]>A[k+1]:
+                    A[k], A[k+1] = A[k+1], A[k]
+        return A
+
+
+
+def buble_sort(A):
+    N=len(A)
+    for i in range(1, N):
+        for k in range(0, N-i):
+            if A[k]>A[k+1]:
+                A[k], A[k+1]= A[k+1],A[k]
+    return A
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def binary_search(A:list, key:int, start:int, stop:int):
+    if start>stop:
+        return False
+    middle=(start+stop)//2
+    if key==A[middle]:
+        return middle
+    elif key<A[middle]:
+        return binary_search(A, key, start, middle-1)
+    else:
+        return binary_search(A, key, middle+1, stop)
+
+
+
+
+def binary_search(A:list, key:int, start:int, stop:int):
+    if start>stop:
+        return False
+    middle(start+stop)//2
+    if key==A[middle]:
+        return middle
+    elif key<A[middle]:
+        return binary_search(A, key, start, middle-1)
+    else:
+        return binary_search(A, key, middle+1, stop)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def insert_sort(A):
+    N=len(A)
+    for top in range(1, N):
+        k=top
+        while k > 0 and A[k-1] > A[k]:
+            A[k], A[k-1] = A[k-1], A[k]
+            k-=1
+    return A
+
+
+
+def insert_sort(A):
+    N=len(A)
+    for top in range(1, N):
+        k=top
+        while k>0 and A[k-1]>A[k]:
+            A[k], A[k-1]=A[k-1], A[k]
+            k-=1
+    return A
+
+
+
+
+
+
+
+
+
+
+
+
+def choise_sort(A):
+    N=len(A)
+    for pos in range(0, N-1):
+        for k in range(pos+1, N):
+            if A[k]<A[pos]:
+                A[k], A[pos]=A[pos], A[k]
+    return A
+
+
+def choise_sort(A):
+    N=len(A)
+    for pos in range(0, N-1):
+        for k in range(pos+1, N):
+            if A[k]<A[pos]:
+                A[k], A[pos]=A[pos], A[k]
+    return A
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def buble_sort(A):
+    N=len(A)
+    for i in range(1, N):
+        for k in range(0, N-i):
+            if A[k]>A[k+1]:
+                A[k], A[k+1]= A[k+1],A[k]
+    return A
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def binary_search(A:list, key:int, start:int, stop:int):
+    if start>stop:
+        return False
+    middle=(start+stop)//2
+    if key==A[middle]:
+        return middle
+    elif key<A[middle]:
+        return binary_search(A, key, start, middle-1)
+    else:
+        return binary_search(A, key, middle+1, stop)
+
+
+
+def binary_search(A:list, key:int, start:int, stop:int):
+    if start>stop:
+        return False
+    middle=(start+stop)//2
+    if key==A[middle]:
+        return middle
+    elif key<A[middle]:
+        return binary_search(A, key, start, middle-1)
+    else:
+        return binary_search(A, key, middle+1, stop)
+
+
+def binary_search(A:list, key:int, start:int, stop:int):
+    if start>stop:
+        return False
+    middle=(start+stop)//2
+    if key==A[middle]:
+        return middle
+    elif key<A[middle]:
+        return binary_search(A, key, start, middle-1)
+    else:
+        return binary_search(A, key, middle+1, stop)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def buble_sort(A):
+    N=len(A)
+    for i in range(1, N):
+        for k in range(0, N-i):
+            if A[k]>A[k+1]:
+                A[k], A[k+1]=A[k+1], A[k]
+    return A
+
+
+
+def buble_sort(A):
+    N=len(A)
+    for i in range(1, N):
+        for k in range(0, N-i):
+            if A[k]>A[k+1]:
+                A[k], A[k+1]=A[k+1], A[k]
+    return A
+
+
+
+
+
+
+
+
+
+
+
+def choise_sort(A):
+    N=len(A)
+    for pos in range(0, N-1):
+        for k in range(pos+1, N):
+            if A[k]<A[pos]:
+                A[k], A[pos]=A[pos], A[k]
+    return A
+
+def choise_sort(A):
+    N=len(A)
+    for pos in range(0, N-1):
+        for k in range(pos+1, N):
+            if A[k]<A[pos]:
+                A[k], A[pos]=A[pos], A[k]
+    return A
+
+
+
+
+
+
+
+
+
+
+
+
+def insert_sort(A):
+    N=len(A)
+    for top in range(1, N):
+        k=top
+        while k>0 and A[k-1]>A[k]:
+            A[k], A[k-1]=A[k-1], A[k]
+            k-=1
+    return A
+
+
+
+
+def insert_sort(A):
+    N=len(A)
+    for top in range(1, N):
+        k=top
+        while k>0 and A[k-1]>A[k]:
+            A[k], A[k-1]=A[k-1], A[k]
+            k-=1
+    return A
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def binary_search(A:list, key:int, start:int, stop:int):
+    if start>stop:
+        return False
+    middle=(start+stop)//2
+    if key==A[middle]:
+        return middle
+    elif key<A[middle]:
+        return binary_search(A, key, start, middle-1)
+    else:
+        return binary_search(A, key, middle+1, stop)
+
+
+def binary_search(A:lis, key:int, start:int, stop:int):
+    if start>stop:
+        return False
+    middle=(start+stop)//2
+    if key==A[middle]:
+        return middle
+    elif key<A[middle]:
+        return binary_search(A, key, start, middle-1)
+    else:
+        return binary_search(A, key, middle+1, stop)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def buble_sort(A):
+    N=len(A)
+    for i in range(1, N):
+        for k in range(0, N-i):
+            if A[k]>A[k+1]:
+                A[k], A[k+1]=A[k+1], A[k]
+    return A
+
+
+
+
+def buble_sort(A):
+    N=len(A)
+    for i in range(1, N):
+        for k in range(0, N-i):
+            if A[k]>A[k+1]:
+                A[k], A[k+1]=A[k+1], A[k]
+    return A
 
 
 
@@ -383,6 +1151,41 @@ def choise_sort(A):
                 A[k], A[pos]=A[pos], A[k]
     return A
 
+def choise_sort(A):
+    N=len(A)
+    for pos in range(0, N-1):
+        for k in range(pos+1, N):
+            if A[k]<A[pos]:
+                A[k], A[pos]=A[pos], A[k]
+    return A
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 def insert_sort(A):
@@ -390,7 +1193,7 @@ def insert_sort(A):
     for top in range(1, N):
         k=top
         while k>0 and A[k-1]>A[k]:
-            A[k], A[k-1] = A[k-1], A[k]
+            A[k], A[k-1]=A[k-1], A[k]
             k-=1
     return A
 
@@ -398,100 +1201,7 @@ def insert_sort(A):
     N=len(A)
     for top in range(1, N):
         k=top
-        while k > 0 and A[k-1] > A[k]:
-            A[k], A[k-1] = A[k-1], A[k]
+        while k>0 and A[k-1]>A[k]:
+            A[k], A[k-1]=A[k-1], A[k]
             k-=1
-    return A
-
-
-
-
-
-
-
-
-
-
-
-
-def buble_sort(A):
-    N=len(A)
-    for i in range(1, N):
-        for k in range(0, N+i):
-            if A[k]>A[k+1]:
-                A[k], A[k+1]=A[k+1], A[k]
-    return A
-
-        def buble_sort(A):
-            N=Len(A)
-            for i in range(1, N):
-                for k in range(0, N-i):
-                    if A[k]>A[k+1]:
-                        A[k], A[k+1]=A[k+1], A[k]
-            return A
-
-
-
-
-
-
-
-
-
-
-def buble_sort(A):
-    N=len(A)
-    for i in range(1, N):
-        for k in range(0, N-i):
-            if A[k]>A[k+1]:
-                A[k], A[k+1] = A[k+1], A[k]
-    return A
-
-
-
-
-
-
-
-    def insert_sort(A):
-        N=len(A)
-        for top in range(1, N):
-            k=top
-            while k>0 and A[k-1]>A[k]:
-                A[k], A[k-1]= A[k-1], A[k]
-                k-=1
-        return A
-
-
-def insert_sort(A):
-    N=len(A)
-    for top in range(1, N):
-        k=top
-        while k > 0 and A[k-1] > A[k]:
-            A[k], A[k-1] = A[k-1], A[k]
-            k-=1
-    return A
-
-
-
-
-
-
-def choise_sort(A):
-    N=len(A)
-    for pos in range(0, N-1):
-        for k in range(pos+1, N):
-            if A[k]<A[pos]:
-                A[k], A[pos] = A[pos], A[k]
-    return A
-
-
-
-
-def choise_sort(A):
-    N=len(A)
-    for pos in range(0, N-1):
-        for k in range(pos+1, N):
-            if A[k]<A[pos]:
-                A[k], A[pos] = A[pos], A[k]
     return A
--- алгоритм.py
+++ алгоритм.py
@@ -46,3 +46,53 @@ def binary_search(A:list, key:int, start:int, stop:int):
          return binary_search(A, key, start, middle-1)
     else:
         return binary_search(A, key, middle+1, stop)
+
+A = [1,12,45,65,86,91,92,100,111]
+start = 0
+stop = len(A)
+key = 45
+a = 55
+
+# s = binary_search(A, a, start, stop)
+#
+#
+# print(s)
+
+
+
+
+#
+"""steck
+модуль описавающий структуру данных -стек
+push(1)#вход в очередь
+push(2)
+push(3)
+is_empty()
+False
+pop()#выход оттуда
+3
+pop()
+2
+pop()
+1
+is_empty()
+True
+"""
+_stack=[]
+
+def push(x):
+    _stack.append(x)
+
+def pop():
+    x=_stack.pop()
+    return x
+
+def clear ():
+    _stack.clear()
+
+def is_empty():
+    return True #len(_stack)==0
+
+if __name__ == "__main__" :
+    import doctest
+    doctest.testmod()
